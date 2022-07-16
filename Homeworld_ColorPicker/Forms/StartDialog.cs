03188b15c408717201971fce21f27cc42803fcb1
joshua-naymie <jnaymie@gmail.com>
2022-07-16 15:44:16 -0600

    Add project files.

---
+++ Homeworld_ColorPicker/Forms/StartDialog.cs
@@ -0,0 +1,10 @@
+namespace Homeworld_ColorPicker
+{
+    public partial class StartDialog : Form
+    {
+        public StartDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
