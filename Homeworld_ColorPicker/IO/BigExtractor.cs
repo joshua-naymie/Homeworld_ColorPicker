5fb47582e43abe2f36f027dac46d299b15136f29
joshua-naymie <jnaymie@gmail.com>
2022-07-23 19:32:47 -0600

    Added BigExtractor class and example Homeworld stuff

---
+++ Homeworld_ColorPicker/IO/BigExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworld_ColorPicker.IO
+{
+    internal class BigExtractor
+    {
+        private const
+        string ARCHIVE_PARAM_FORMAT = "{0}{1} -a {2}",
+               ARCHIVE_PATH_FORMAT = "{0}{1}";
+
+        private const
+        string DIR_GAME_DATA = @"\Data",
+               FILE_HW2_BIG = @"\Homeworld2.big";
+        
+        public static void ExtractBigFile(string homeworldRoot, string toolkitRoot, string outputPath)
+        {
+
+        }
+    }
+}
